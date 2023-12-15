4be713a4e4c406882b650bbae0fcdf636cb1e564
Kipkiruie <elishak551@gmail.com>
2023-12-15 13:28:46 -0800

    push and pall

---
+++ pall.c
@@ -0,0 +1,56 @@
+#include "monthy.h"
+/**
+ * f_pall - prints the stack
+ * @head: Head of stack
+ * @counter: no used
+ * Retuen: no return
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void) counter;
+
+	h = *head;
+
+	if (h == NULL);
+		return;
+	while(h)
+	{
+	printf("%d",h->)
+		h = h -> next;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
